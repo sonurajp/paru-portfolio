67dcdd3b1afc1926870f135dd2c3401d3bdfa967
sonurajp <sonurajp49@gmail.com>
2026-02-09 16:24:52 +0530

    add orange arrow

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box, Typography } from "@mui/material";
 import FooterArrow from "../../../assets/FooterArrow.png";
+import RightOrange from "../../../assets/RightOrange.svg";
 import "./Footer.css";
 const email = "parvathy.26@outlook.com";
 const gmailLink = `https://mail.google.com/mail/?view=cm&fs=1&to=${email}`;
@@ -21,6 +22,17 @@ const Footer = () => (
       mt="150px"
       gap="45px"
       onClick={() => window.open(gmailLink, "_blank")}
+      sx={{
+        ".rightOrange": {
+          display: "none",
+        },
+        "&:hover .rightOrange": {
+          display: "block",
+        },
+        "&:hover .footerArrow": {
+          display: "none", // optional if you want to hide first arrow
+        },
+      }}
     >
       <Typography
         fontSize={"160px"}
@@ -35,6 +47,16 @@ const Footer = () => (
       <img
         src={FooterArrow}
         onClick={() => window.open(gmailLink, "_blank")}
+        className="footerArrow"
+        style={{
+          width: "115px",
+          height: "94px",
+        }}
+      />
+      <img
+        src={RightOrange}
+        onClick={() => window.open(gmailLink, "_blank")}
+        className="rightOrange"
         style={{
           width: "115px",
           height: "94px",
@@ -54,31 +76,36 @@ const Footer = () => (
         parvathy.26@outlook.com &nbsp;+91 9500485529
       </Typography>
       <Box ml={1} mt="-.2%">
-        <a
+        <Typography
+          component="a"
           href="https://behance.net/"
           target="_blank"
-          style={{
+          sx={{
             color: "#111",
             textDecoration: "none",
-            marginRight: 16,
+            marginRight: 2,
             fontSize: "14px",
             fontWeight: 500,
+            "&:hover": { color: "#ff5832", cursor: "pointer" },
           }}
         >
           Behance
-        </a>
-        <a
+        </Typography>
+        <Typography
+          component="a"
           href="https://www.linkedin.com/in/parvathy-t-a-148a43104/"
           target="_blank"
-          style={{
+          sx={{
             color: "#111",
             textDecoration: "none",
+            marginRight: 2,
             fontSize: "14px",
             fontWeight: 500,
+            "&:hover": { color: "#ff5832", cursor: "pointer" },
           }}
         >
           Linkedin
-        </a>
+        </Typography>
       </Box>
       <Typography
         sx={{
